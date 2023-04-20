92806d8e76166474aeed1989e218bdbd2155c9c1
Neves888 <helen.vneves@gmail.com>
2023-04-20 17:30:28 -0300

    finalmente add req 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,18 +1,14 @@
-from src.models.ingredient import Ingredient # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    ingredient_1 = Ingredient("ovo")
-    assert ingredient_1.name == "ovo"
-    assert ingredient_1.restrictions == set()
-    assert ingredient_1.__repr__() == "Ingredient('ovo')"
-
-    ingredient_2 = Ingredient("farinha")
-    assert ingredient_2.name == "farinha"
-    assert ingredient_2.restrictions == set()
-    assert ingredient_2.__repr__() == "Ingredient('farinha')"
-
-    ingredient_3 = Ingredient("massa")
-    assert ingredient_2.__hash__() != ingredient_1.__hash__()
-    assert ingredient_2.__eq__(ingredient_3) is True
+    ingredient = Ingredient("presunto")
+    assert ingredient.name == "presunto"
+    assert ingredient.restrictions == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.ANIMAL_DERIVED
+     }
+    assert ingredient.__repr__() == "Ingredient('presunto')"
+    assert ingredient.__hash__() == hash("presunto")
+    assert ingredient.__eq__(ingredient)
