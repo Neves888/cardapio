c30e9d334c69d00acfd90c97f6fe54ebd7af479a
Neves888 <helen.vneves@gmail.com>
2023-04-20 16:17:37 -0300

    testa test outra vez

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -4,11 +4,12 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 # Req 1
 def test_ingredient():
     ingredient_1 = Ingredient("ovo")
-    ingredient_2 = Ingredient("manteiga")
+    ingredient_2 = Ingredient("farinha")
+    ingredient_3 = Ingredient("massa")
 
     assert ingredient_1.restrictions == set()
     assert ingredient_1.name == "ovo"
     assert repr(ingredient_1) == "Ingredient('ovo')"
 
-    assert hash(ingredient_1) == hash(ingredient_1)
-    assert hash(ingredient_1) != hash(ingredient_2)
+    assert hash(ingredient_2) == hash(ingredient_3)
+    assert hash(ingredient_2) != hash(ingredient_1)
