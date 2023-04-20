eb46591b11e7a96fb54fe9dd6f7829f26cba9cb5
Neves888 <helen.vneves@gmail.com>
2023-04-20 16:33:14 -0300

    refatora test para ver se passa

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,15 +1,18 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
     ingredient_1 = Ingredient("ovo")
-    ingredient_2 = Ingredient("farinha")
-    ingredient_3 = Ingredient("massa")
-
-    assert ingredient_1.restrictions == set()
     assert ingredient_1.name == "ovo"
-    assert repr(ingredient_1) == "Ingredient('ovo')"
+    assert ingredient_1.restrictions == []
+    assert ingredient_1.__repr__() == "Ingredient('ovo')"
 
-    assert hash(ingredient_2) == hash(ingredient_3)
-    assert hash(ingredient_2) != hash(ingredient_1)
+    ingredient_2 = Ingredient("farinha")
+    assert ingredient_2.name == "farinha"
+    assert ingredient_2.restrictions == []
+    assert ingredient_2.__repr__() == "Ingredient('farinha')"
+
+    ingredient_3 = Ingredient("massa")
+    assert ingredient_2.__hash__() != ingredient_1.__hash__()
+    assert ingredient_2.__eq__(ingredient_3) is True
