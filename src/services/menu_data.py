3901658c029aaadd7968560ca532f6817bcfd8b6
Neves888 <helen.vneves@gmail.com>
2023-04-20 20:11:51 -0300

    add req 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,22 @@
+from models.ingredient import Ingredient
+from src.models.dish import Dish
+import pandas as pd
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = dict()
+        dishes = {}
+        read_file = pd.read_csv(source_path)
+        for dish, price, ingredient, recipe_amount in read_file.itertuples(
+                index=False):
+            list = Dish(dish, price)
+            ingredient = Ingredient(ingredient)
+            if list not in dishes:
+                dishes[list] = list
+            dishes[list].add_ingredient_dependency(
+                ingredient,
+                recipe_amount
+            )
+        self.dishes = set(dishe for dishe in dishes)
