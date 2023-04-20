22f29dac92571c9ad17c97f58c880593bdc13789
Neves888 <helen.vneves@gmail.com>
2023-04-20 16:01:08 -0300

    testa req 1-teste 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,12 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient_1 = Ingredient("ovo")
+    ingredient_2 = Ingredient("manteiga")
+
+    assert ingredient_1.restrictions == set()
+    assert ingredient_1.name == "ovo"
+    assert repr(ingredient_1) == "Ingredient('ovo')"
+
+    assert hash(ingredient_1) == hash(ingredient_1)
+    assert hash(ingredient_1) != hash(ingredient_2)
